cc0af7b1f8fda14cb2735d17666fcb450e852398
studio-banana-officiel <martinezekiel278@gmail.com>
2026-05-04 00:47:09 -0400

    Update notification message and player attributes

--- main.lua
+++ main.lua
@@ -1,6 +1,12 @@
-print("--- STUDIO BANANA LOADED ---")
+print("--- STUDIO BANANA V2 ACTIVÉE ---")
+
+-- Notification de succès
 game.StarterGui:SetCore("SendNotification", {
     Title = "Studio Banana",
-    Text = "Le script est actif !",
+    Text = "Mode Vitesse et Saut Activé !",
     Duration = 5
 })
+
+-- Tes premières fonctions de "Cheats"
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100 -- Tu vas courir super vite
+game.Players.LocalPlayer.Character.Humanoid.JumpPower = 150 -- Tu vas sauter très haut
